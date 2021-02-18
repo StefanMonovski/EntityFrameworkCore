c1c5a52c76de754eca7dce831f8016a41532c8ba
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-18 22:05:38 +0200

    Add VillainNames

---
+++ 01.ADO.NET/02.VillainNames/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _02.VillainNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection selectVillainNamesConnection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true");
+            using (selectVillainNamesConnection)
+            {
+                selectVillainNamesConnection.Open();
+                SqlCommand selectVillainNamesCommand = new SqlCommand(
+                    @"SELECT V.Name, COUNT(MV.VillainId) AS MinionsCount
+                        FROM Villains AS V
+                        JOIN MinionsVillains AS MV ON V.Id = MV.VillainId
+                        GROUP BY V.Id, V.Name
+                        HAVING COUNT(MV.VillainId) > 3
+                        ORDER BY COUNT(MV.VillainId);",
+                    selectVillainNamesConnection);
+                using (selectVillainNamesCommand)
+                {
+                    SqlDataReader reader = selectVillainNamesCommand.ExecuteReader();
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            string name = (string)reader["Name"];
+                            int minionsCount = (int)reader["MinionsCount"];
+                            Console.WriteLine($"{name} - {minionsCount}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
