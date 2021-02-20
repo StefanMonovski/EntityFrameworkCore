3099416eeba6310c9b61f22fc0d904ef4610202b
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-20 17:06:14 +0200

    Add PrintAllMinionNames

---
+++ 01.ADO.NET/07.PrintAllMinionNames/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _07.PrintAllMinionNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection printMinionNamesConnection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true");
+            using (printMinionNamesConnection)
+            {
+                printMinionNamesConnection.Open();
+
+                List<string> minionNames = new List<string>();
+                SqlCommand selectMinionNamesCommand = new SqlCommand(
+                    @"SELECT Name
+                        FROM Minions;",
+                    printMinionNamesConnection);
+                using (selectMinionNamesCommand)
+                {
+                    SqlDataReader reader = selectMinionNamesCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        minionNames.Add((string)reader["Name"]);
+                    }
+                }
+
+                for (int i = 0; i < minionNames.Count / 2; i++)
+                {
+                    Console.WriteLine(minionNames[0 + i]);
+                    Console.WriteLine(minionNames[minionNames.Count - 1 - i]);
+                }
+                if (minionNames.Count % 2 != 0)
+                {
+                    Console.WriteLine(minionNames[minionNames.Count / 2]);
+                }
+            }      
+        }
+    }
+}
