555e76aa5aba7cea08621f8360f8d2558c59f138
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-26 11:32:03 +0200

    Add EmployeesWithSalaryOver50000

---
+++ 03.EntityFrameworkIntroduction/03.EmployeesWithSalaryOver50000/Program.cs
@@ -0,0 +1,32 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new { x.FirstName, x.Salary })
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
