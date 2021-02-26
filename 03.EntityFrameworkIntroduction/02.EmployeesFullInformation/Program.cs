9bcfd2f2a22838183b82df3bb40aa8fea97f3aa7
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-26 11:06:04 +0200

    Add EmployeesFullInformation

---
+++ 03.EntityFrameworkIntroduction/02.EmployeesFullInformation/Program.cs
@@ -0,0 +1,38 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new { x.EmployeeId, x.FirstName, x.LastName, x.MiddleName, x.JobTitle, x.Salary })
+                .OrderBy(x => x.EmployeeId)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                if (employee.MiddleName == null)
+                {
+                    sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.JobTitle} {employee.Salary:f2}");
+                }
+                else
+                {
+                    sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+                }
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
