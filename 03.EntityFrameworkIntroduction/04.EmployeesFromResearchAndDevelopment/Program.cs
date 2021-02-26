365c4ba5a78a4dd3c8670c6e168b03fda9923242
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-26 13:09:36 +0200

    Add EmployeesFromResearchAndDevelopment

---
+++ 03.EntityFrameworkIntroduction/04.EmployeesFromResearchAndDevelopment/Program.cs
@@ -0,0 +1,33 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x => new { x.FirstName, x.LastName, x.Department.Name, x.Salary })
+                .Where(x => x.Name == "Research and Development")
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Name} - ${employee.Salary:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
