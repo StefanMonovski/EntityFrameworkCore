fd9dc5ba9b96491598ac8604cf4a98a8ae5d3aa3
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-27 18:57:49 +0200

    Add AddressesByTown

---
+++ 03.EntityFrameworkIntroduction/07.AddressesByTown/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetAddressesByTown(context));
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var addresses = context.Addresses
+                .Include(x => x.Town)
+                .Include(x => x.Employees)
+                .Select(x => new
+                {
+                    x.AddressText,
+                    townName = x.Town.Name,
+                    employeesCount = x.Employees.Count
+                })
+                .OrderByDescending(x => x.employeesCount)
+                .ThenBy(x => x.townName)
+                .ThenBy(x => x.AddressText)
+                .Take(10)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var address in addresses)
+            {
+                sb.AppendLine($"{address.AddressText}, {address.townName} - {address.employeesCount} employees");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
