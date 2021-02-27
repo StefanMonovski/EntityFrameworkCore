89546a3c10bd6e78d1b6e6778b3bc49e5fecf01f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-27 12:41:27 +0200

    Add EmployeesAndProjects

---
+++ 03.EntityFrameworkIntroduction/06.EmployeesAndProjects/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetEmployeesInPeriod(context));
+        }
+
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Include(x => x.EmployeesProjects)
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    managerFirstName = x.Manager.FirstName,
+                    managerLastName = x.Manager.LastName,
+                    projects = x.EmployeesProjects
+                    .Select(y => new { y.Project.Name, y.Project.StartDate, y.Project.EndDate })
+                })
+                .Where(x => x.projects.Any(y => y.StartDate >= new DateTime(2001, 1, 1) && y.StartDate <= new DateTime(2003, 1, 1)))
+                .Take(10)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} - Manager: {employee.managerFirstName} {employee.managerLastName}");
+                foreach (var project in employee.projects)
+                {
+                    if (project.EndDate == null)
+                    {
+                        sb.AppendLine($"--{project.Name} - {project.StartDate} - not finished");
+                    }
+                    else
+                    {
+                        sb.AppendLine($"--{project.Name} - {project.StartDate} - {project.EndDate}");
+                    }                    
+                }
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
