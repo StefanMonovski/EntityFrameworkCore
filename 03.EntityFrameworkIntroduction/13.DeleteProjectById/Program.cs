19af080e29b15272ae9138bff5f2b0f0fe09e0c7
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-01 17:14:41 +0200

    Add DeleteProjectById

---
+++ 03.EntityFrameworkIntroduction/13.DeleteProjectById/Program.cs
@@ -0,0 +1,46 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(DeleteProjectById(context));
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            var employeesProject = context.EmployeesProjects
+                .Where(x => x.ProjectId == 2);
+
+            foreach (var employeeProject in employeesProject)
+            {
+                context.EmployeesProjects.Remove(employeeProject);
+            }
+            context.SaveChanges();
+
+            var project = context.Projects
+                .First(x => x.ProjectId == 2);
+
+            context.Projects.Remove(project);
+            context.SaveChanges();
+
+            var projects = context.Projects
+                .Select(x => x.Name)
+                .Take(10)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var projectName in projects)
+            {
+                sb.AppendLine(projectName);
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
