4c04b0c83c217cb7cc643237c415f8ed7a0ba2cf
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-01 14:14:27 +0200

    Add DepartmentsWithMoreThan5Employees

---
+++ 03.EntityFrameworkIntroduction/09.DepartmentsWithMoreThan5Employees/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetDepartmentsWithMoreThan5Employees(context));
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            var departments = context.Departments
+                .Include(x => x.Employees)
+                .Select(x => new
+                {
+                    x.Name,
+                    managerFirstName = x.Manager.FirstName,
+                    managerLastName = x.Manager.LastName,
+                    employees = x.Employees
+                    .Select(x => new { x.FirstName, x.LastName, x.JobTitle })
+                    .OrderBy(x => x.FirstName)
+                    .ThenBy(x => x.LastName)
+                    .ToList()
+                })
+                .Where(x => x.employees.Count > 5)
+                .OrderBy(x => x.employees.Count)
+                .ThenBy(x => x.Name)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var department in departments)
+            {
+                sb.AppendLine($"{department.Name} - {department.managerFirstName} {department.managerLastName}");
+                foreach (var employee in department.employees)
+                {
+                    sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+                }
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
