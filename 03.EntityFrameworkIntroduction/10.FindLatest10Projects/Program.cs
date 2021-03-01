47ef2233a6d54be80973398bba437a3f09db16a7
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-01 15:02:21 +0200

    Add FindLatest10Projects

---
+++ 03.EntityFrameworkIntroduction/10.FindLatest10Projects/Program.cs
@@ -0,0 +1,35 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            Console.WriteLine(GetLatestProjects(context));
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var projects = context.Projects
+                .Select(x => new { x.Name, x.Description, x.StartDate })
+                .OrderByDescending(x => x.StartDate)
+                .Take(10)
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var project in projects)
+            {
+                sb.AppendLine(project.Name);
+                sb.AppendLine(project.Description);
+                sb.AppendLine(project.StartDate.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
