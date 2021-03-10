6c834ffe0fe100925058b7bb1709544ccbebd222
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-10 16:32:33 +0200

    Add AlbumsInfo

---
+++ 05.LINQ/02.AlbumsInfo/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicHub.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace MusicHub
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new MusicHubDbContext();
+            MusicHubDbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(ExportAlbumsInfo(context, int.Parse(Console.ReadLine())));
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            var albums = context.Albums
+                .Include(x => x.Producer)
+                .Include(x => x.Songs)
+                .ThenInclude(x => x.Writer)
+                .Where(x => x.ProducerId == producerId)
+                .Select(x => new
+                {
+                    AlbumName = x.Name,
+                    AlbumReleaseDate = x.ReleaseDate,
+                    ProducerName = x.Producer.Name,
+                    AlbumSongs = x.Songs.Select(x => new
+                    {
+                        SongName = x.Name,
+                        SongPrice = x.Price,
+                        WriterName = x.Writer.Name
+                    })
+                    .OrderByDescending(x => x.SongName)
+                    .ThenBy(x => x.WriterName)
+                    .ToList(),
+                    AlbumPrice = x.Price
+                })
+                .OrderByDescending(x => x.AlbumPrice)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var album in albums)
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}");
+                sb.AppendLine($"-ReleaseDate: {album.AlbumReleaseDate:MM/dd/yyyy}");
+                sb.AppendLine($"-ProducerName: {album.ProducerName}");
+                if (album.AlbumSongs.Count() > 0)
+                {
+                    sb.AppendLine("-Songs:");
+                    int i = 1;
+                    foreach (var song in album.AlbumSongs)
+                    {
+                        sb.AppendLine($"---#{i}");
+                        sb.AppendLine($"---SongName: {song.SongName}");
+                        sb.AppendLine($"---Price: {song.SongPrice:f2}");
+                        sb.AppendLine($"---Writer: {song.WriterName}");
+                        i++;
+                    }
+                }
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
