5601bd598aa3624c28d828d80ca1b7ead2383e7c
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-12 15:38:32 +0200

    Add GoldenBooks

---
+++ 06.AdvancedQuerying/03.GoldenBooks/Data/Configurations/BookConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Data.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.Property(x => x.Title)
+                .IsUnicode();
+
+            builder.Property(x => x.Description)
+                .IsUnicode();
+        }
+    }
+}
---
+++ 06.AdvancedQuerying/03.GoldenBooks/Program.cs
@@ -0,0 +1,36 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetGoldenBooks(context));
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var titles = context.Books
+                .Where(x => x.Copies < 5000 && Enum.Parse<EditionType>("Gold") == x.EditionType)
+                .OrderBy(x => x.BookId)
+                .Select(x => x.Title)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var title in titles)
+            {
+                sb.AppendLine(title);
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
