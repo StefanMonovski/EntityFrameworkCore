78bb7ae51d89455d7922ad9b2e2b9fabaa18f67e
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-12 15:38:15 +0200

    Add AgeRestriction

---
+++ 06.AdvancedQuerying/02.AgeRestriction/Program.cs
@@ -0,0 +1,36 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetBooksByAgeRestriction(context, Console.ReadLine()));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            var titles = context.Books
+                .Where(x => Enum.Parse<AgeRestriction>(command, true) == x.AgeRestriction)
+                .Select(x => x.Title)
+                .OrderBy(x => x)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var title in titles)
+            {
+                sb.AppendLine(title);
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
