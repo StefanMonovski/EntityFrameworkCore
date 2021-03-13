221b54c7410aa75b5dab15b3e65d932282521f15
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 14:57:50 +0200

    Add TotalBookCopies

---
+++ 06.AdvancedQuerying/12.TotalBookCopies/Program.cs
@@ -0,0 +1,39 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(CountCopiesByAuthor(context));
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            var authors = context.Authors
+                .Select(x => new
+                {
+                    Name = x.FirstName + " " + x.LastName,
+                    Copies = x.Books.Sum(x => x.Copies)
+                })
+                .OrderByDescending(x => x.Copies)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var author in authors)
+            {
+                sb.AppendLine($"{author.Name} - {author.Copies}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
