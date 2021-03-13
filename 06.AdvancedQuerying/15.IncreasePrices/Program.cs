36bd736cdf86beb742d82a24434ec5e07ea53da4
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 15:35:56 +0200

    Add IncreasePrices

---
+++ 06.AdvancedQuerying/15.IncreasePrices/Program.cs
@@ -0,0 +1,32 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(x => x.ReleaseDate.Value.Year < 2010)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
