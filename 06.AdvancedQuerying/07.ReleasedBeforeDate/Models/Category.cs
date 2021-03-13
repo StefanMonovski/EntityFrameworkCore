b8e80236c2e15938e9399c8e0336e411d00d2a8a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 13:26:23 +0200

    Add ReleasedBeforeDate

---
+++ 06.AdvancedQuerying/07.ReleasedBeforeDate/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookShop.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            CategoryBooks = new HashSet<BookCategory>();
+        }
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<BookCategory> CategoryBooks { get; set; }
+    }
+}
---
+++ 06.AdvancedQuerying/07.ReleasedBeforeDate/Program.cs
@@ -0,0 +1,39 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetBooksReleasedBefore(context, Console.ReadLine()));
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var maxReleaseDate = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            var books = context.Books
+                .Where(x => x.ReleaseDate < maxReleaseDate)
+                .OrderByDescending(x => x.ReleaseDate)
+                .Select(x => new { x.Title, x.EditionType, x.Price })
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} - {book.EditionType} - ${book.Price:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
