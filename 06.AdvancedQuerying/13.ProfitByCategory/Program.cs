334ccf7978dc603873205e44827a29cd5fcb383f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 15:06:50 +0200

    Add ProfitByCategory

---
+++ 06.AdvancedQuerying/13.ProfitByCategory/Program.cs
@@ -0,0 +1,39 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetTotalProfitByCategory(context));
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var categories = context.Categories
+                .Select(x => new
+                {
+                    x.Name,
+                    Profit = x.CategoryBooks.Sum(x => x.Book.Copies * x.Book.Price)
+                })
+                .OrderByDescending(x => x.Profit)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var category in categories)
+            {
+                sb.AppendLine($"{category.Name} ${category.Profit:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
