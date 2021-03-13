c801d92c6ed0f8de55e9bae544bdcc2ac834809a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 15:42:54 +0200

    Add RemoveBooks

---
+++ 06.AdvancedQuerying/16.RemoveBooks/Program.cs
@@ -0,0 +1,37 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(RemoveBooks(context));
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(x => x.Copies < 4200)
+                .ToList();
+
+            int removed = 0;
+            foreach (var book in books)
+            {
+                context.Remove(book);
+                removed++;
+            }
+
+            context.SaveChanges();
+            return removed;
+        }
+    }
+}
