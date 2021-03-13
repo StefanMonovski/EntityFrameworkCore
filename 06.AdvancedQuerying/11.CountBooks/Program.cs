78c00e8e443c01cc8eebaeed3645a86f91c4f622
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 14:09:23 +0200

    Add CountBooks

---
+++ 06.AdvancedQuerying/11.CountBooks/Program.cs
@@ -0,0 +1,29 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(CountBooks(context, int.Parse(Console.ReadLine())));
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            int count = context.Books
+                .Where(x => x.Title.Length > lengthCheck)
+                .Count();
+
+            return count;
+        }
+    }
+}
