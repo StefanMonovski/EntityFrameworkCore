ad1e28b09fa76753a5b53f7a0557c1d9e3925a54
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 13:58:37 +0200

    Add BookSearchByAuthor

---
+++ 06.AdvancedQuerying/10.BookSearchByAuthor/Models/Book.cs
@@ -0,0 +1,44 @@
+﻿using BookShop.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookShop.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+            BookCategories = new HashSet<BookCategory>();
+        }
+
+        [Key]
+        public int BookId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+        
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public int Copies { get; set; }
+
+        public decimal Price { get; set; }
+
+        public EditionType EditionType { get; set; }
+
+        public AgeRestriction AgeRestriction { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        public ICollection<BookCategory> BookCategories { get; set; }
+    }
+}
+ 
---
+++ 06.AdvancedQuerying/10.BookSearchByAuthor/Program.cs
@@ -0,0 +1,36 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetBooksByAuthor(context, Console.ReadLine()));
+        }
+
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            var books = context.Books
+                .Where(x => x.Author.LastName.Substring(0, input.Length).ToLower() == input.ToLower())
+                .OrderBy(x => x.BookId)
+                .Select(x => new { x.Title, AuthorName = x.Author.FirstName + " " + x.Author.LastName })
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} ({book.AuthorName})");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
