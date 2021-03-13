843aae21f1b4aec44d795c638e83bab3b1ffc134
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-13 13:42:00 +0200

    Add BookSearch

---
+++ 06.AdvancedQuerying/09.BookSearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using BookShop.Data;
+using BookShop.Initializer;
+using BookShop.Models.Enums;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace BookShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new BookShopContext();
+            DbInitializer.ResetDatabase(context);
+
+            Console.WriteLine(GetBookTitlesContaining(context, Console.ReadLine()));
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            var titles = context.Books
+                .Select(x => x.Title)
+                .Where(x => x.ToLower().Contains(input.ToLower()))
+                .OrderBy(x => x)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var title in titles)
+            {
+                sb.AppendLine(title);
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
