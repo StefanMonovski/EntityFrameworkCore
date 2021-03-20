a0a7b5287160346eb1fd7b6c0b2314ff2b026247
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-20 14:42:38 +0200

    Add FastFood.Data

---
+++ 07.AutoMappingObjects/02.FastFood.Data/Migrations/20210319113637_UpdateEnumType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FastFood.Data.Migrations
+{
+    public partial class UpdateEnumType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Orders",
+                type: "VARCHAR(30)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(30)");
+        }
+    }
+}
