1c7a9a6fc7fc67b7fe37a1175d18a00a995a837b
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-21 19:03:02 +0200

    Add ImportUsers

---
+++ 08.JSONProcessing/02.ImportUsers/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Collections.Generic;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            Console.WriteLine(ImportUsers(context, inputJson));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<IEnumerable<User>>(inputJson);
+
+            context.Users.AddRange(users);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
