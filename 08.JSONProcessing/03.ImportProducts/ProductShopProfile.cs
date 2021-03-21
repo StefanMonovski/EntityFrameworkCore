8ec38c9e7bc24925cd131e68e521fc87ad595acc
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-21 23:45:14 +0200

    Add ImportProducts

---
+++ 08.JSONProcessing/03.ImportProducts/ProductShopProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<CategoryProduct, Category>();
+
+            CreateMap<CategoryProduct, Product>();
+        }
+    }
+}
---
+++ 08.JSONProcessing/03.ImportProducts/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Collections.Generic;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/products.json");
+            Console.WriteLine(ImportProducts(context, inputJson));
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(inputJson);
+
+            context.Products.AddRange(products);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
