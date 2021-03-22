14119f754b2a4470679e1e5a96fe701d3588466f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-22 11:45:42 +0200

    Add ImportCategories

---
+++ 08.JSONProcessing/04.ImportCategories/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/categories.json");
+            Console.WriteLine(ImportCategories(context, inputJson));
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(inputJson)
+                .Where(x => x.Name != null);
+
+            context.Categories.AddRange(categories);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
