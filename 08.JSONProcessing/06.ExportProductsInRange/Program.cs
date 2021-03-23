60a9f99c1028ca524672bdab453730af55a794df
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-23 18:22:43 +0200

    Add ExportProductsInRange

---
+++ 08.JSONProcessing/06.ExportProductsInRange/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Collections.Generic;
+using System.Linq;
+using ProductShop.DataTransferObjects;
+using Newtonsoft.Json.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureCreated();
+
+            File.WriteAllText("../../../Exports/products-in-range.json", GetProductsInRange(context));
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Select(x => new ProductDto
+                {
+                    Name = x.Name,
+                    Price = x.Price,
+                    Seller = (x.Seller.FirstName + " " + x.Seller.LastName)
+                })
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .OrderBy(x => x.Price)
+                .ToList();
+
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Formatting.Indented
+            };
+            var productsJson = JsonConvert.SerializeObject(products, jsonSerializerSettings);
+            return productsJson;
+        }
+    }
+}
