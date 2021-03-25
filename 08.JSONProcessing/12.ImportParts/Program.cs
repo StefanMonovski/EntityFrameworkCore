5b8b4bd60d4a6b846bd8e05db661ecefea04f7d2
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-25 18:40:13 +0200

    Add ImportParts

---
+++ 08.JSONProcessing/12.ImportParts/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using CarDealer.Data;
+using CarDealer.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/parts.json");
+            Console.WriteLine(ImportParts(context, inputJson));
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var parts = JsonConvert.DeserializeObject<IEnumerable<Part>>(inputJson)
+                .Where(x => context.Suppliers.Select(x => x.Id).Contains(x.SupplierId));
+
+            context.Parts.AddRange(parts);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}.";
+        }
+    }
+}
