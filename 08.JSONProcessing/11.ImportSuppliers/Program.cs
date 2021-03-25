5fb73ee43991d6358c52331ad5bdc2314e8ce905
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-25 12:43:13 +0200

    Add ImportSuppliers

---
+++ 08.JSONProcessing/11.ImportSuppliers/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using CarDealer.Data;
+using CarDealer.Models;
+using System.Collections.Generic;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/suppliers.json");
+            Console.WriteLine(ImportSuppliers(context, inputJson));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<IEnumerable<Supplier>>(inputJson);
+
+            context.Suppliers.AddRange(suppliers);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}.";
+        }
+    }
+}
