9d68f222349857f2f26d3241d152bebe8194d4f6
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-30 11:50:53 +0300

    Add ImportCustomers

---
+++ 08.JSONProcessing/14.ImportCustomers/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using CarDealer.Data;
+using CarDealer.Models;
+using System.Collections.Generic;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/customers.json");
+            Console.WriteLine(ImportCustomers(context, inputJson));
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            var customers = JsonConvert.DeserializeObject<IEnumerable<Customer>>(inputJson);
+
+            context.Customers.AddRange(customers);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}.";
+        }
+    }
+}
