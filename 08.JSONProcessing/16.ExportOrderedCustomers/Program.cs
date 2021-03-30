5c3afc51be6ca7627f54ce84c51f21bc10232882
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-30 13:13:39 +0300

    Add ExportOrderedCustomers

---
+++ 08.JSONProcessing/16.ExportOrderedCustomers/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using CarDealer.Data;
+using CarDealer.Models;
+using System.Collections.Generic;
+using System.Linq;
+using CarDealer.DataTransferObjects;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            File.WriteAllText("../../../Exports/ordered-customers.json", GetOrderedCustomers(context));
+        }
+
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .OrderBy(x => x.BirthDate)
+                .ThenBy(x => x.IsYoungDriver)
+                .Select(x => new CustomerDto
+                {
+                    Name = x.Name,
+                    BirthDate = x.BirthDate.ToString("dd/MM/yyyy"),
+                    IsYoungDriver = x.IsYoungDriver,
+                })
+                .ToList();
+
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            };
+            var customersJson = JsonConvert.SerializeObject(customers, jsonSerializerSettings);
+            return customersJson;
+        }
+    }
+}
