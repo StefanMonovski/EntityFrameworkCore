79bbc26c1bd96e59149b5c960f114b4586c1888a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-30 19:00:52 +0300

    Add ExportSalesWithAppliedDiscount

---
+++ 08.JSONProcessing/21.ExportSalesWithAppliedDiscount/DataTransferObjects/CustomerPurchasesDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DataTransferObjects
+{
+    public class CustomerPurchasesDto
+    {
+        public string FullName { get; set; }
+
+        public int BoughtCars { get; set; }
+
+        public decimal SpentMoney { get; set; }
+    }
+}
---
+++ 08.JSONProcessing/21.ExportSalesWithAppliedDiscount/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using CarDealer.Data;
+using CarDealer.Models;
+using System.Collections.Generic;
+using System.Linq;
+using CarDealer.DataTransferObjects;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            File.WriteAllText("../../../Exports/sales-discounts.json", GetSalesWithAppliedDiscount(context));
+        }
+
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            var sales = context.Sales
+                .Take(10)
+                .Select(x => new
+                {
+                    car = new CarDto
+                    {
+                        Make = x.Car.Make,
+                        Model = x.Car.Model,
+                        TravelledDistance = x.Car.TravelledDistance
+                    },
+                    customerName = x.Customer.Name,
+                    Discount = x.Discount.ToString("f2"),
+                    price = x.Car.PartCars.Sum(x => x.Part.Price).ToString("f2"),
+                    priceWithDiscount = (x.Car.PartCars.Sum(x => x.Part.Price) - x.Car.PartCars.Sum(x => x.Part.Price) * x.Discount / 100).ToString("f2"),
+                })
+                .ToList();
+
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+            var salesJson = JsonConvert.SerializeObject(sales, jsonSerializerSettings);
+            return salesJson;
+        }
+    }
+}
