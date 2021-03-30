bac08afb9d7be97f5d97440fccb3031dd6695286
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-03-30 12:25:28 +0300

    Add ImportSales

---
+++ 08.JSONProcessing/15.ImportSales/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using CarDealer.Data;
+using CarDealer.Models;
+using System.Collections.Generic;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            string inputJson = File.ReadAllText("../../../Datasets/sales.json");
+            Console.WriteLine(ImportSales(context, inputJson));
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            var sales = JsonConvert.DeserializeObject<IEnumerable<Sale>>(inputJson);
+
+            context.Sales.AddRange(sales);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}.";
+        }
+    }
+}
