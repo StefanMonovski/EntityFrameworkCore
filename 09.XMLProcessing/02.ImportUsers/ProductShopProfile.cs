8b38a3ea9a94fe21120a2cf164407b46cd629eb8
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-01 22:55:34 +0300

    Add ImportUsers

---
+++ 09.XMLProcessing/02.ImportUsers/ProductShopProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ProductShop.DataTransferObjects;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<CategoryProduct, Category>();
+
+            CreateMap<CategoryProduct, Product>();
+
+            CreateMap<ImportUserDto, User>();
+        }
+    }
+}
---
+++ 09.XMLProcessing/02.ImportUsers/Program.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DataTransferObjects;
+using ProductShop.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureCreated();
+
+            Mapper.Initialize(x => x.AddProfile(new ProductShopProfile()));
+
+            string inputXml = File.ReadAllText("../../../Datasets/users.xml");
+            System.Console.WriteLine(ImportUsers(context, inputXml));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(List<ImportUserDto>), new XmlRootAttribute("Users"));
+            var usersDto = (List<ImportUserDto>)serializer.Deserialize(new StringReader(inputXml));
+
+            var users = Mapper.Map<List<User>>(usersDto);
+            context.Users.AddRange(users);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
