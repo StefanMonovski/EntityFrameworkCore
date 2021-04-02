64b9819cfb35305b7f90d2004cd91cb3028fdffc
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-02 13:32:19 +0300

    Add ExportCategoriesByProductsCount

---
+++ 09.XMLProcessing/08.ExportCategoriesByProductsCount/DataTransferObjects/UserSoldProductsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.DataTransferObjects
+{
+    [XmlType("User")]
+    public class UserSoldProductsDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlArray("soldProducts")]
+        public List<SoldProductDto> SoldProducts { get; set; }
+    }
+}
+
---
+++ 09.XMLProcessing/08.ExportCategoriesByProductsCount/Program.cs
@@ -0,0 +1,45 @@
+﻿using ProductShop.Data;
+using ProductShop.DataTransferObjects;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureCreated();
+
+            File.WriteAllText("../../../Exports/categories-by-products.xml", GetCategoriesByProductsCount(context));
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var categories = context.Categories
+                .Select(x => new CategoryDto
+                {
+                    Name = x.Name,
+                    Count = x.CategoryProducts.Count(),
+                    AveragePrice = x.CategoryProducts.Average(x => x.Product.Price),
+                    TotalRevenue = x.CategoryProducts.Sum(x => x.Product.Price),
+                })
+                .OrderByDescending(x => x.Count)
+                .ThenBy(x => x.TotalRevenue)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            var serializer = new XmlSerializer(typeof(List<CategoryDto>), new XmlRootAttribute("Categories"));
+            serializer.Serialize(new StringWriter(sb), categories, namespaces);
+
+            return sb.ToString().Trim();
+        }
+    }
+}
