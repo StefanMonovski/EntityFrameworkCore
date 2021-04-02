6b8b18d66802eae6428abd4e2125943bf11ea623
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-02 09:51:46 +0300

    Add ImportProducts

---
+++ 09.XMLProcessing/03.ImportProducts/Program.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DataTransferObjects;
+using ProductShop.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static readonly MapperConfiguration config = new MapperConfiguration(x => x.AddProfile(new ProductShopProfile()));
+        static readonly IMapper mapper = config.CreateMapper();
+
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText("../../../Datasets/products.xml");
+            System.Console.WriteLine(ImportProducts(context, inputXml));
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(List<ImportProductDto>), new XmlRootAttribute("Products"));
+            var productsDto = (List<ImportProductDto>)serializer.Deserialize(new StringReader(inputXml));
+            
+            var products = mapper.Map<List<Product>>(productsDto);
+            context.Products.AddRange(products);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
