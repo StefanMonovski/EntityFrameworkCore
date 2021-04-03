3af28b0b26bbc0ce143131b8ed157a947b362b6a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-03 16:08:07 +0300

    Add ImportSales

---
+++ 09.XMLProcessing/15.ImportSales/CarDealerProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CarDealer.DataTransferObjects;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<PartCar, Part>();
+
+            CreateMap<PartCar, Car>();
+
+            CreateMap<ImportSupplierDto, Supplier>();
+
+            CreateMap<ImportPartIdDto, Part>();
+
+            CreateMap<ImportCarDto, Car>()
+                .ForMember(x => x.TravelledDistance, x => x.MapFrom(x => x.TraveledDistance));
+
+            CreateMap<ImportCustomerDto, Customer>();
+
+            CreateMap<ImportSaleDto, Sale>();
+        }
+    }
+}
---
+++ 09.XMLProcessing/15.ImportSales/Program.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DataTransferObjects;
+using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static readonly MapperConfiguration config = new MapperConfiguration(x => x.AddProfile(new CarDealerProfile()));
+        static readonly IMapper mapper = config.CreateMapper();
+
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText("../../../Datasets/sales.xml");
+            Console.WriteLine(ImportSales(context, inputXml));
+        }
+        
+        public static string ImportSales(CarDealerContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(List<ImportSaleDto>), new XmlRootAttribute("Sales"));
+            var salesDto = (List<ImportSaleDto>)serializer.Deserialize(new StringReader(inputXml));
+
+            var sales = mapper.Map<List<Sale>>(salesDto);
+            context.Sales.AddRange(sales.Where(x => context.Cars.Select(x => x.Id).Contains(x.CarId)));
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
