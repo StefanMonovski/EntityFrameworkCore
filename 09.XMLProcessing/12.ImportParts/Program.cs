ce11612e8030462fc2e044dac75cf8b18d53ffbd
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-03 14:35:53 +0300

    Add ImportParts

---
+++ 09.XMLProcessing/12.ImportParts/Program.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DataTransferObjects;
+using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static readonly MapperConfiguration config = new MapperConfiguration(x => x.AddProfile(new CarDealerProfile()));
+        static readonly IMapper mapper = config.CreateMapper();
+
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText("../../../Datasets/parts.xml");
+            Console.WriteLine(ImportParts(context, inputXml));
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(List<ImportPartDto>), new XmlRootAttribute("Parts"));
+            var partsDto = (List<ImportPartDto>)serializer.Deserialize(new StringReader(inputXml));
+
+            var parts = mapper.Map<List<Part>>(partsDto);
+            context.Parts.AddRange(parts.Where(x => context.Suppliers.Select(x => x.Id).Contains(x.SupplierId)));
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
