6db3eca66eda8616deb158b97cfdcf2885aaefbf
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-03 17:57:17 +0300

    Add ExportCarsFromMakeBMW

---
+++ 09.XMLProcessing/17.ExportCarsFromMakeBMW/Program.cs
@@ -0,0 +1,45 @@
+﻿using CarDealer.Data;
+using CarDealer.DataTransferObjects;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            File.WriteAllText("../../../Exports/bmw-cars.xml", GetCarsFromMakeBmw(context));
+        }
+
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var cars = context.Cars
+                .Where(x => x.Make == "BMW")
+                .Select(x => new CarAttributesDto
+                {
+                    Id = x.Id,
+                    Model = x.Model,
+                    TravelledDistance = x.TravelledDistance,
+                })
+                .OrderBy(x => x.Model)
+                .ThenByDescending(x => x.TravelledDistance)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            var serializer = new XmlSerializer(typeof(List<CarAttributesDto>), new XmlRootAttribute("cars"));
+            serializer.Serialize(new StringWriter(sb), cars, namespaces);
+
+            return sb.ToString().Trim();
+        }
+    }
+}
