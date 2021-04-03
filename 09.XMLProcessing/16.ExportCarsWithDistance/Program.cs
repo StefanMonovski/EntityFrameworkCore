18aedfc3a310056e3a0a9a140b9c7a0b304a0127
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-03 17:40:13 +0300

    Add ExportCarsWithDistance

---
+++ 09.XMLProcessing/16.ExportCarsWithDistance/Program.cs
@@ -0,0 +1,46 @@
+﻿using CarDealer.Data;
+using CarDealer.DataTransferObjects;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            File.WriteAllText("../../../Exports/cars.xml", GetCarsWithDistance(context));
+        }
+
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            var cars = context.Cars
+                .Select(x => new CarDto
+                {
+                    Make = x.Make,
+                    Model = x.Model,
+                    TravelledDistance = x.TravelledDistance,
+                })
+                .Where(x => x.TravelledDistance >= 2000000)
+                .OrderBy(x => x.Make)
+                .ThenBy(x => x.Model)
+                .Take(10)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            var serializer = new XmlSerializer(typeof(List<CarDto>), new XmlRootAttribute("cars"));
+            serializer.Serialize(new StringWriter(sb), cars, namespaces);
+
+            return sb.ToString().Trim();
+        }
+    }
+}
