e576e77c2dc6d1cb63a95259a5d75df7e2738f17
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-03 14:21:29 +0300

    Add ImportSuppliers

---
+++ 09.XMLProcessing/11.ImportSuppliers/CarDealerProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CarDealer.DataTransferObjects;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<PartCar, Part>();
+
+            CreateMap<PartCar, Car>();
+
+            CreateMap<ImportSupplierDto, Supplier>();
+        }
+    }
+}
---
+++ 09.XMLProcessing/11.ImportSuppliers/Program.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DataTransferObjects;
+using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        static readonly MapperConfiguration config = new MapperConfiguration(x => x.AddProfile(new CarDealerProfile()));
+        static readonly IMapper mapper = config.CreateMapper();
+
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+
+            string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            Console.WriteLine(ImportSuppliers(context, inputXml));
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(List<ImportSupplierDto>), new XmlRootAttribute("Suppliers"));
+            var suppliersDto = (List<ImportSupplierDto>)serializer.Deserialize(new StringReader(inputXml));
+
+            var suppliers = mapper.Map<List<Supplier>>(suppliersDto);
+            context.Suppliers.AddRange(suppliers);
+            int count = context.SaveChanges();
+
+            return $"Successfully imported {count}";
+        }
+    }
+}
