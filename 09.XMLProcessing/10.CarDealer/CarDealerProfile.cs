f1609ac34a4f2a97669a901c84aac531302038fd
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-04-03 14:20:27 +0300

    Add CarDealer

---
+++ 09.XMLProcessing/10.CarDealer/CarDealerProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<PartCar, Part>();
+
+            CreateMap<PartCar, Car>();
+        }
+    }
+}
---
+++ 09.XMLProcessing/10.CarDealer/Program.cs
@@ -0,0 +1,13 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureCreated();
+        }
+    }
+}
