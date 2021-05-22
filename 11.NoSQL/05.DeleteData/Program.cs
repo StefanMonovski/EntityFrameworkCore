12e02d665438cc023f76c7618de084dba9cd80b1
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-05-22 17:59:48 +0300

    Add DeleteData

---
+++ 11.NoSQL/05.DeleteData/Program.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace _05.DeleteData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("NoSQL");
+            var collection = database.GetCollection<BsonDocument>("Articles");
+
+            List<BsonDocument> articlesBeforeDelete = collection.Find(new BsonDocument()).ToList();
+            foreach (var article in articlesBeforeDelete)
+            {
+                if (int.Parse(article.GetElement("rating").Value.AsString) <= 50)
+                {
+                    collection.DeleteOne(article);
+                }
+            }
+            
+            List<BsonDocument> articlesAfterDelete = collection.Find(new BsonDocument()).ToList();
+            foreach (var article in articlesAfterDelete)
+            {
+                Console.WriteLine(article.GetElement("name").Value);
+            }
+        }
+    }
+}
