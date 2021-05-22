2a7308dbc871df176280d802b8668b1c5ba9eb91
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-05-22 16:51:47 +0300

    Add ReadData

---
+++ 11.NoSQL/02.ReadData/Program.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace _02.ReadData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("NoSQL");
+            var collection = database.GetCollection<BsonDocument>("Articles");
+
+            List<BsonDocument> articles = collection.Find(new BsonDocument()).ToList();
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article.GetElement("name").Value);
+            }
+        }
+    }
+}
