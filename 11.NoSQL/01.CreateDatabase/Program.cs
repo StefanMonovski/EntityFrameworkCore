d9c23df146a8ac9c911f1cb9e22d7c83444b682f
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-05-22 16:03:18 +0300

    Add CreateDatabase

---
+++ 11.NoSQL/01.CreateDatabase/Program.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01.CreateDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("NoSQL");
+            var collection = database.GetCollection<BsonDocument>("Articles");
+
+            BsonArray articles = BsonSerializer.Deserialize<BsonArray>(File.ReadAllText(@"Datasets\articles.json"));
+            foreach (var article in articles)
+            {
+                collection.InsertOne(article.AsBsonDocument);
+            }
+        }
+    }
+}
