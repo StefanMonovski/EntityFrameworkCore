e74a7a2635b78e33dc74fac376daf85c5e3bc84a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-05-22 17:24:18 +0300

    Add UpdateData

---
+++ 11.NoSQL/04.UpdateData/Program.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace _04.UpdateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("NoSQL");
+            var collection = database.GetCollection<BsonDocument>("Articles");
+
+            List<BsonDocument> articles = collection.Find(new BsonDocument()).ToList();
+            foreach (var article in articles)
+            {
+                var update = Builders<BsonDocument>.Update.Set("rating", (int.Parse(article.GetElement("rating").Value.AsString) + 10).ToString());
+                collection.UpdateOne(article, update);
+            }
+        }
+    }
+}
