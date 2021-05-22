e603fe4cfbef6a7d9504aadf4312425e4b70bfc6
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-05-22 17:01:32 +0300

    Add CreateArticle

---
+++ 11.NoSQL/03.CreateArticle/Program.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace _03.CreateArticle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("NoSQL");
+            var collection = database.GetCollection<BsonDocument>("Articles");
+
+            BsonDocument article = new BsonDocument()
+            {
+                { "author", "Steve Jobs"},
+                { "date", "05-05-2005" },
+                { "name", "The story of Apple" },
+                { "rating", "50" }
+            };
+
+            collection.InsertOne(article);
+        }
+    }
+}
